fc6fa88c2bbafbdbbf60fb9e025479692ce61f98
manishtmtmt <manisht.mt.mt@gmail.com>
2022-11-08 15:21:13 +0530

    displayed game history

---
+++ src/components/History.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const History = ({ history, moveTo, currentMove }) => {
+  return (
+    <ul>
+      {history.map((_, move) => (
+        <li key={move}>
+          <button
+            style={{
+              fontWeight: move === currentMove ? "bold" : "normal",
+            }}
+            onClick={() => {
+              moveTo(move);
+            }}
+          >
+            {move === 0 ? "Go to game start" : `Go to move #${move}`}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default History;
